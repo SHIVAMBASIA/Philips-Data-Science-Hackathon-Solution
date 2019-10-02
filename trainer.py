5518b62c10ed64fbb420f91e1538ce661ae1f079
SHIVAM BASIA <56087185+SHIVAMBASIA@users.noreply.github.com>
2019-10-03 02:07:52 +0530

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,44 @@
+# Importing Required Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Training Dataset
+dataset = pd.read_csv('train.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# SciKit Learn Preprocessing Library Import
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# SciKit Learn Classifier Library Import
+from sklearn.svm import SVC
+svc = svm.SVC()
+parameters = {'kernel':('linear','rbf'), 'C':[1,120]}
+classifier=GridSearchCV(svc,parameters)
+#classifier = SVC(kernel = ('linear','rbf'))
+classifier.fit(X, y)
+
+# Test dataset
+test = pd.read_csv('test.csv')
+test = test.drop('id',1)
+X_test = test.iloc[:, :].values
+X_test = sc.transform(X_test)
+
+y_pred = classifier.predict(X_test)
+
+
+test = pd.read_csv('test.csv')
+i = pd.DataFrame(test, columns=['id'])
+l = pd.DataFrame(y_pred, columns=['price_range'])
+i.join(l)
+
+res = pd.concat([i, l], axis=1)
+
+res.to_csv('result.csv',encoding='utf-8', index=False)
+
+result = pd.read_csv('result.csv')
+
+
